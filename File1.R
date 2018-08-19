f5aae6f479ff7584ab68bf37b61b794a6929c897
pvarunc <varun.p18161@iimtrichy.ac.in>
2018-08-19 19:58:32 +0530

    setup 19-09-18

--- File1.R
+++ File1.R
@@ -1,1 +1,13 @@
 mtcars
+
+git config --global user.email "varun.p18161@iimtrichy.ac.in"
+git config --global user.name "pvarunc"
+
+install.packages('car')
+plot(women)
+?women
+?mean
+x=1:100
+x
+y = seq(1,100, by=2)
+y
